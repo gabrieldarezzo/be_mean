fe5541e6b5e62296d3261af8a56b05f358d68b28
Gabriel Darezzo <gabrieldarezzo@yahoo.com>
2016-06-27 16:14:20 -0300

    FIX cors with another Methods (PUT/DELETE)

--- api/index.php
+++ api/index.php
@@ -1,11 +1,36 @@
 <?php
 
-//ADD GitHub CORS, This avoid: No 'Access-Control-Allow-Origin'
-header('Access-Control-Allow-Origin: http://gabrieldarezzo.github.io');  
+
+## ATENÇÃO leia o .htaccess desta budega ## 
+header('Content-Type: application/json; charset=utf-8');
+header('Access-Control-Allow-Origin: *');  
+header('Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE');  
+header('Access-Control-Allow-Headers: *');  
+header('Access-Control-Max-Age: 86400');
+
+/*
+//Resolve porem duplica, resolvi pelo .htaccess.... 
+if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+    if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD']) && (
+            $_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD'] == 'POST' ||
+            $_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD'] == 'DELETE' ||
+            $_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD'] == 'PUT' )) {
+        header('Access-Control-Allow-Origin: *');
+        header("Access-Control-Allow-Credentials: true");
+        header('Access-Control-Allow-Headers: X-Requested-With');
+        header('Access-Control-Allow-Headers: Content-Type');
+        header('Access-Control-Allow-Methods: POST, GET, OPTIONS, DELETE, PUT'); // http://stackoverflow.com/a/7605119/578667
+        header('Access-Control-Max-Age: 86400');
+    }
+    exit;
+}
+*/
+
+
 
 require 'vendor/autoload.php';
  
-header('Content-Type: application/json; charset=utf-8');
+
 // var_dump($_SERVER['REQUEST_METHOD']); //GET, POST, PUT, DELETE
 
 //Tem o script sql.sql....
@@ -26,9 +51,12 @@ function getConnection() {
 	} else {
 		//PRODUÇÃO MANO!11!
 		
-		//Na produção ta certo...
 		
-
+		$db = new PDO("mysql:host=mysql.inwork.com.br;dbname=inwork02;charset=utf8", 'inwork02', 'w4f3x2');  
+		$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+		$db->exec("set names utf8"); //Garante UTF em versão < 5.3
+		
+		return $db;
 	}
 		
 }
@@ -56,7 +84,7 @@ $app->delete('/user/:id', function ($id) {
 	$stmt->bindParam(':id', $id, PDO::PARAM_INT);   
 	
 	if($stmt->execute()){
-		print json_encode(array('action' => true));		
+		print json_encode(array('action' => true, 'tipo' => 'delete'));		
 	} else {
 		print json_encode(array('action' => false));
 	}
@@ -83,7 +111,7 @@ $app->put('/user/', function () {
 	$stmt->bindParam(':type', $user->type, PDO::PARAM_STR);       
 	$stmt->bindParam(':id', $user->id, PDO::PARAM_INT);   
 	$stmt->execute();
-	print json_encode(array('action' => true));
+	print json_encode(array('action' => true, 'tipo' => 'put'));
 });
 
 
